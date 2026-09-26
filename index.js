b7ae7834de390250e9098276aa07346436bbc254
Solo <solomonmaguta492@gmail.com>
2026-09-25 17:51:23 -1000

    added a function that returns the largest of two numbers

--- index.js
+++ index.js
@@ -16,3 +16,11 @@ function convertToUpperCase(text) {
 console.log(convertToUpperCase(`hello`));
 console.log(convertToUpperCase(`good morning`));
 
+// Function 3 : 
+// Returns the larger of two numbers
+function findMaximum(num1, num2) {
+    return Math.max(num1, num2) ;
+}
+console.log(findMaximum(5 , 9));
+console.log(findMaximum(100 ,45 ));
+console.log(findMaximum(7 , 7));
