3d1100f754a2ac7e9e3f0d3aa4fc92599feeec6e
Solo <solomonmaguta492@gmail.com>
2026-09-25 17:42:15 -1000

    added a function that converts string to uppercase

--- index.js
+++ index.js
@@ -7,3 +7,12 @@ return amount * 0.10;
 let tax = calculateTax(100)
 console.log(tax)
 
+// Function 2 : convertToUpperCase
+// Converts a string to uppercase
+
+function convertToUpperCase(text) {
+    return text.toUpperCase();
+}
+console.log(convertToUpperCase(`hello`));
+console.log(convertToUpperCase(`good morning`));
+
